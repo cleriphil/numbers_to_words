0248af9d77befbf3c151f1563bc371839d8f024a
jordan carlson & cleri philiastides <student@epicodus.com>
2015-04-21 16:40:52 -0700

    Add and pass spec that returns a double digit divisible by ten and greater than 19

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,11 +2,18 @@ class Fixnum
   define_method(:numbers_to_words) do
     single_digits = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
     teen_digits = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
+    tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
 
-    if self.>(9)
-    teen_digits.fetch(self)
+    if self.>(19).&((self.%(10)).==(0))
+      tens.fetch(self)
+    elsif self.<(10)
+      single_digits.fetch(self)
+    elsif self.>(9)
+      teen_digits.fetch(self)
   else
-    single_digits.fetch(self)
   end
   end
 end
+
+
+# self.to_s.size
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,7 +3,9 @@ require('numbers_to_words')
 
 
 # returns a single digit as a written number
-# returns a double digit as a written number
+# returns a teen double digit as a written number
+# returns a double digit greater than 19 as a written number
+# returns a double digit divisible by ten and greater than 19
 
 
 
@@ -11,7 +13,13 @@ describe('Fixnum#numbers_to_words') do
   it("returns a single digit as a written number") do
     expect(5.numbers_to_words()).to(eq("five"))
   end
-  it("returns a double digit as a written number") do
+  it("returns a teen double digit as a written number") do
     expect(19.numbers_to_words()).to(eq("nineteen"))
   end
+  it("returns a double digit divisible by ten and greater than 19") do
+    expect(30.numbers_to_words()).to(eq("thirty"))
+  end
+#  it("returns a double digit greater than 19 as a written number") do
+#    expect(32.numbers_to_words()).to(eq("thirty two"))
+#  end
 end
